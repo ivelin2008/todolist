99aed3761189e0428e9b47bd94573139818bfac7
Ivelin Ivanov <Ivelin>
2017-11-08 04:20:08 +0000

    First Commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TodoList</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+    <footer class='text-center' id="footer">
+        <p>Copyright &copy; 2017 TodoList</p>
+    </footer>
+</body>
+</html>
